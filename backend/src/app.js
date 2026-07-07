d0a5c7ff3009f10ed6e3298de5a11240cfc321fb
Tanvir-art <darknessboytanvir690@gmail.com>
2026-07-07 21:56:45 +0600

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -38,7 +38,7 @@ app.use(helmet());
 // }));
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'https://buddy-front-git-main-tanvir-haans-projects.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
